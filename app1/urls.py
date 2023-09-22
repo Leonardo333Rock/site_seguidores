b7a423d2afff0fb1e5587c72de2034387af531e6
LeonardoMatheus <onardo_mateus@hotmail.com>
2023-09-22 16:42:42 -0300

    links facebook criados

--- app1/urls.py
+++ app1/urls.py
@@ -7,5 +7,10 @@ urlpatterns = [
     path('curtidas_instagram',views.Curtida_insta,name='curtidas_instagram'),
     path('visualizacao_insta',views.Visualizacao_insta,name='visualizacao_insta'),
 
+    path('seguidores_face',views.Seguidores_face,name='seguidores_face'),
+    path('curtidas_face',views.Curtidas_face,name='curtidas_face'),
+    path('visualizacao_face',views.Visualizacao_face,name='visualizacao_face'),
+
+
 
 ]
--- app1/views.py
+++ app1/views.py
@@ -4,6 +4,8 @@ from django.shortcuts import render
 def Home(request):
     return render(request,'home.html')
 
+            #INSTAGRAM
+
 def Seguidores_instagram(request):
     return render(request,'instagram/seguidores.html')
 
@@ -12,6 +14,16 @@ def Curtida_insta(request):
 
 def Visualizacao_insta(request):
     return render(request,'instagram/visualizacao_insta.html')
+
+            # FACEBOOK
     
+def Seguidores_face(request):
+    return render(request,'facebook/seguidores_face.html')
+
+def Curtidas_face(request):
+    return render(request,'facebook/curtidas_face.html')
+
+def Visualizacao_face(request):
+    return render(request,'facebook/visualizacao_face.html')
 
 
